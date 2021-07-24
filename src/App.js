ab2c04afbaf41dec03f789cd27b2ec0794351a87
IchrakJh <87020396+IchrakJh@users.noreply.github.com>
2021-07-24 13:32:43 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import react from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+import Profile from './profile/profile';
+
+
+
+function App() {
+  const style = { border: "solid 1px black", maxWidth: "100vw" };
+  const handleName = () => {
+    alert("fullname");
+  };
+  return (
+    <>
+      <Profile
+        fullName="nametest"
+        bio="biotest"
+        profession="professiontest"
+        onClick={handleName}
+      >
+        <img src="https://image.flaticon.com/icons/png/512/147/147144.png"></img>
+      </Profile>
+    </>
+  );
+}
+export default App;
